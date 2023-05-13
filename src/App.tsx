464d758942593ee660d848dc4480b331fe6878f5
byErraco <erraco.wow@gmail.com>
2023-05-13 10:29:08 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { Button } from "./components/ui/button";
+import { Input } from "./components/ui/input";
+import Login from "./pages/Login";
+
+export default function App() {
+  return (
+    <main className="overflow-hidden rounded-[0.5rem] border bg-background shadow-xl">
+      <div className="container relative hidden h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+        <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
+          <div
+            className="absolute inset-0 bg-cover"
+            style={{
+              backgroundImage: `url("https://images.unsplash.com/photo-1590069261209-f8e9b8642343?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1376&q=80")`,
+            }}
+          />
+        </div>
+        <div className="lg:p-8">
+          <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+            <div className="flex flex-col space-y-2 text-center">
+              <h1 className="text-2xl font-semibold tracking-tight">
+                Login :)
+              </h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
---
+++ src/pages/Login.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div className="bg-gray-950 rounded-lg border-solid border-[0.8px] text-slate-200 leading-6 overflow-hidden">
+      hola
+    </div>
+  );
+};
+
+export default Login;
